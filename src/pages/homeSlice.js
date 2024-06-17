f6427752f1adef4d9de3d4316ebe3a3b25d367cc
Dr. Victor Vasconcelos <32229843+drvictorvs@users.noreply.github.com>
2024-06-17 08:40:24 -0300

    Update homeSlice.js
    Fix link to allRepositoriesSlice.

--- src/pages/homeSlice.js
+++ src/pages/homeSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import headers from '../allRepositoriesSlice';
+import headers from './allRepositoriesSlice';
 
 const githubUsername = "drvictorvs";
 
